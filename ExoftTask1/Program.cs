c8b725304dfe7d83b0e93f59b91e964a7ec35103
Kulyniak <80394011+Kulyniak@users.noreply.github.com>
2022-07-26 12:20:14 +0300

    Add project files.

---
+++ ExoftTask1/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+namespace ExoftTask1
+{
+
+
+
+    class FightArena
+    {
+        
+       
+        static void Main()
+
+        {
+           
+
+        }
+
+    }
+
+}
+
