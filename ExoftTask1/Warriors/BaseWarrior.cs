a1155c696812f57368bb4926c9143a41930dd1bb
Kulyniak <80394011+Kulyniak@users.noreply.github.com>
2022-07-26 22:29:00 +0300

    added attack and defence

--- ExoftTask1/Warriors/BaseWarrior.cs
+++ ExoftTask1/Warriors/BaseWarrior.cs
@@ -12,6 +12,54 @@ namespace ExoftTask1.Warriors
        public int Armor { get; set; }
         public int Punch { get; set; }
 
+        public bool isAlive()
+        {
+            if (Health > 0)
+            {
+                return true;
+            }
+            else { return false; }
+        }
+
+        public void Defence(int enemy_damage)
+        {
+            if (Armor > 0)
+            {
+                if (enemy_damage > 1)
+                {
+                    if ((enemy_damage / 2) > Armor)
+                    
+                    {
+                        Health -= enemy_damage - Armor;
+                        Armor = 0;
+                    }
+                    Armor -= enemy_damage / 2;
+                    Health -= enemy_damage - (enemy_damage / 2); 
+                }
+                else if (enemy_damage == 1)
+                {
+                    Armor -= enemy_damage;
+                }
+            }
+            Console.WriteLine($"Armor left: {Armor}");
+        }
+
+        public int Damage()
+        {
+            if (Armor > 0)
+            {
+                return Punch;
+            }
+            else
+            {
+                if (Punch > 1)
+                {
+                    Punch--;
+                }
+                return Punch;
+            }
+        }
+
         public virtual void AddSuperPower(ISuperPower super, int power) { }
     }
 }
