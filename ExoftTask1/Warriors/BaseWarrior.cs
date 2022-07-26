b069252010e9462d450cc1dbb5498190d5ea03be
Kulyniak <80394011+Kulyniak@users.noreply.github.com>
2022-07-26 17:28:58 +0300

    Added BaseWarrior and derived classes

---
+++ ExoftTask1/Warriors/BaseWarrior.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExoftTask1.Warriors
+{
+    internal abstract class BaseWarrior
+    {
+        int Health = 100;
+        int Armor { get; set; }
+        int Punch { get; set; }
+    }
+}
